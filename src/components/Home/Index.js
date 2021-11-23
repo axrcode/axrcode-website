6e9697f09067bafeb93725fd0086e2ddea6141d1
axrcode <axrcode@gmail.com>
2021-11-22 20:20:56 -0600

    Configuración inicial del proyecto

---
+++ src/components/Home/Index.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+
+function Index() 
+{
+    return(
+        <Fragment>
+            <h1>Hola Mundo</h1>
+        </Fragment>
+    );
+}
+
+export { Index };
