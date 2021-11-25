5766b91f9dc43f05aebbde74c6d922f26a8d809c
axrcode <axrcode@gmail.com>
2021-11-24 21:17:45 -0600

    Add Bootstrap

--- src/components/Home/Index.js
+++ src/components/Home/Index.js
@@ -4,7 +4,17 @@ function Index()
 {
     return(
         <Fragment>
-            <h1>Hola Mundo</h1>
+            <div className="container">
+                <h1>Hola Mundo</h1>
+
+                <div className="row">
+                    <div className="col-md-6 offset-md-3">
+                        <div className="d-grid gap-2">
+                            <button className="btn btn-primary" type="button">Button</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </Fragment>
     );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+import * as bootstrap from 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
