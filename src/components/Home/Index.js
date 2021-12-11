5f321a8910ede216be750a315d9fa1fd6795bd5c
axrcode <axrcode@gmail.com>
2021-12-10 22:43:01 -0600

    Add TailwindCSS
    Remove Bootstrap

--- src/components/Home/Index.js
+++ src/components/Home/Index.js
@@ -7,13 +7,7 @@ function Index()
             <div className="container">
                 <h1>Hola Mundo</h1>
 
-                <div className="row">
-                    <div className="col-md-6 offset-md-3">
-                        <div className="d-grid gap-2">
-                            <button className="btn btn-primary" type="button">Button</button>
-                        </div>
-                    </div>
-                </div>
+                
             </div>
         </Fragment>
     );
--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-import * as bootstrap from 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
